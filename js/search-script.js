053cd9e15c04112d2d33fc6d540cbaf80e3756ad
Sybren Gjaltema <sybren_gjaltema@msn.com>
2014-02-22 00:12:45 +0100

    Add another button and make page and totalPages ints

--- js/search-script.js
+++ js/search-script.js
@@ -54,18 +54,22 @@ function fetchPage(e) {
                                     )
                                     );
                 }); //end of each loop
-
+                page = parseInt(page);
+                totalPages = parseInt(totalPages);
                 console.log("Total pages : " + totalPages);
                 console.log("Current page: " + page);
                 var pagination = $(".pagination");
                 pagination.empty();
-                pagination.append($("<li>").addClass(page === 1 ? "disabled" : "").append($("<a>").attr("href", "javascript:void(0)").attr("onclick", "fetchPage(" + (parseInt(page) - 1) + ")").html("&laquo;")));
+                pagination.append($("<li>").addClass(page === 1 ? "disabled" : "").append($("<a>").attr("href", "javascript:void(0)").attr("onclick", "fetchPage(" + (page - 1) + ")").html("&laquo;")));
                 pagination.append($("<li>").addClass(page === 1 ? "active" : "").append($("<a>").attr("href", "javascript:void(0)").attr("onclick", "fetchPage(this)").html(page <= 2 ? "1" : ((totalPages - page <= 1) ? (totalPages - 4) : (page - 2)))));
                 if(totalPages > 2){
                     pagination.append($("<li>").addClass(page === 2 ? "active" : "").append($("<a>").attr("href", "javascript:void(0)").attr("onclick", "fetchPage(this)").html(page <= 2 ? "2" : ((totalPages - page <= 1) ? (totalPages - 3) : (page - 1)))));
                 }
+                if(totalPages > 3){
+                    pagination.append($("<li>").addClass(page !== 1 && page !== 2 && page !== parseInt(totalPages) && page !== (parseInt(totalPages) - 1) ? "active" : "").append($("<a>").attr("href", "javascript:void(0)").attr("onclick", "fetchPage(this)").html(totalPages - page <= 2 ? (totalPages-2) : (page > 2 ? page : "3"))));
+                }
 
-                pagination.append($("<li>").addClass(page === totalPages ? "disabled" : "").append($("<a>").attr("href", "javascript:void(0)").attr("onclick", "fetchPage(" + (parseInt(page) + 1) + ")").html("&raquo;")));
+                pagination.append($("<li>").addClass(page === totalPages ? "disabled" : "").append($("<a>").attr("href", "javascript:void(0)").attr("onclick", "fetchPage(" + (page + 1) + ")").html("&raquo;")));
 
 
             }); //end of done function and ajax call
