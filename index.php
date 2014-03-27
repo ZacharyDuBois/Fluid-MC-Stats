03b6f939ecce360a4feb33b7ad5aa8a993b321ac
Zachary DuBois <zacharydubois@icloud.com>
2014-03-27 19:43:20 -0400

    - Index now uses security.php for checks. - Another random span cleanup.

--- index.php
+++ index.php
@@ -3,26 +3,10 @@
   ~ Copyright (c) AccountProductions and Lolmewn 2014. All Rights Reserved.
 -->
 <?php
-if (!file_exists('config.php')) {
-  echo "<h1>config.php missing!</h1>";
-  die();
-}
+include 'inc/security.php';
 include 'config.php';
 
 $navPage = "home";
-
-if ($mysql_host == '' && file_exists("pages/install/")) {
-  $http = ($_SERVER['HTTPS'] ? 'https://' : 'http://');
-  $installLoc = $http . $_SERVER['HTTP_HOST'] . $_SERVER["REQUEST_URI"] . "pages/install/install.php";
-  header("Location: " . $installLoc);
-  die();
-}
-if ($mysql_host == '') {
-  die("Config misconfigured and install folder is not at default location, can't launch website");
-}
-if (file_exists("pages/install/")) {
-  die("Security Issues! Please move or delete the installation folder!");
-}
 ?>
 <html>
 <head>
@@ -73,11 +57,11 @@ include 'inc/navbar.php';
               <div class="input-group input-group-lg">
                 <span class="input-group-addon"><i class="fa fa-user"></i></span>
                 <input name='name' type="text" class="form-control" placeholder="Username">
-                                        <span class="input-group-btn">
-                                            <button type="submit" class="btn btn-default">
-                                              Find <i class="fa fa-chevron-right"></i>
-                                            </button>
-                                        </span>
+                <span class="input-group-btn">
+                  <button type="submit" class="btn btn-default">
+                    Find <i class="fa fa-chevron-right"></i>
+                  </button>
+                </span>
               </div>
             </form>
             <p>Explore...</p>
