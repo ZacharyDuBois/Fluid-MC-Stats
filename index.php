e5bb7cbf9129ec5428e87c6a2d83a17041a4891c
Zachary DuBois <zacharydubois@icloud.com>
2014-11-06 14:25:56 -0500

    Push changes for debugging.

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
-define('LINKBASE', str_replace('index.php', '', $_SERVER['PHP_SELF']));
+
+$base = str_replace('index.php', '', $_SERVER['PHP_SELF']);
+if ($base === '') {
+  $base == '/';
+
+}
+define('LINKBASE', $base);
+var_dump(LINKBASE)
 
 switch (strtolower($_GET['view'])) {
   case '':
