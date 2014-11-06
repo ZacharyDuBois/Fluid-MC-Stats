d5f30067572766d71fc4a81cf3f78665bd179f6c
Zachary DuBois <zacharydubois@icloud.com>
2014-11-06 13:05:26 -0500

    Make sure index is redirected.

--- inc/navbar.php
+++ inc/navbar.php
@@ -12,7 +12,7 @@ include_once __DIR__ . '/../config.php';
     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
       <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span>
     </button>
-    <a class="navbar-brand" href="<?php echo LINKBASE; ?>index" title="Home"><i class="fa <?php echo $fa_icon; ?>"></i> <?php echo $site_name; ?></a>
+    <a class="navbar-brand" href="<?php echo LINKBASE; ?>" title="Home"><i class="fa <?php echo $fa_icon; ?>"></i> <?php echo $site_name; ?></a>
   </div>
   <!-- /Mobile -->
 
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 define('LINKBASE', str_replace('index.php', '', $_SERVER['PHP_SELF']));
 
 switch (strtolower($_GET['view'])) {
-  case '':
+  case '' or 'index':
     include_once 'pages/front-page.php';
     break;
 
@@ -14,4 +14,3 @@ switch (strtolower($_GET['view'])) {
     include_once 'pages/' . strtolower($_GET['view']) . '.php';
     break;
 }
-?>
