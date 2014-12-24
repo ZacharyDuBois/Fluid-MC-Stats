cff388c7dc25589af41c2991eef19209e88192c1
hockeymikey <hockeymikey@live.com>
2014-12-23 19:15:09 -0600

    Update to newer branch that I didn't check from

--- inc/serverstatusui.php
+++ inc/serverstatusui.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Copyright (c) AccountProductions and Sybren Gjaltema, 2014. All rights reserved.
+ * Copyright (c) AccountProductions and Lolmewn 2014. All Rights Reserved.
  */
 
-include_once APPPATH . 'config.php';
+include_once '../config.php';
 include_once 'serverstatus.php';
 
 $serverStatus = new serverstatus($mc_server_ip, $mc_server_port);
@@ -18,13 +18,47 @@ $serverStatus->fetchServerData();
       <img src="<?php if (!empty($mc_custom_icon)) {
         echo($mc_custom_icon);
       } else {
-        echo "$serverStatus->getFavicon();
+        echo $serverStatus->getFavicon();
       } ?>" alt="<?php echo $server_name; ?>&apos;s Icon" class="img-circle server-sidebar-icon">
     </div>
+    <?php if ($serverStatus->isOnline() == true) { ?>
+      <h3 class="mc-online"><i class="fa fa-check"></i> Online!</h3>
+    <?php } elseif ($serverStatus->isDead() == true) { ?>
+      <h3 class="mc-offline"><i class="fa fa-times-circle-o"></i> Offline</h3>
+    <?php } else { ?>
+      <h3 class="mc-unknown"><i class="fa fa-question-circle"></i> Unknown</h3>
+    <?php
+    }
+    ?>
     <hr>
     <p><strong>Name:</strong> <?php echo $server_name; ?></p>
 
     <p><strong>IP:</strong> <?php echo $mc_server_disp_addr; ?></p>
+    <?php if ($serverStatus->isOnline() == true) { ?>
+      <p><strong>Players:</strong> <?php echo $serverStatus->getOnlinePlayerCount() . "/" . $serverStatus->getMaxPlayerCount(); ?></p>
+    <?php } elseif ($serverStatus->isDead() == true) { ?>
+      <p><strong>Players:</strong> ?/?</p>
+    <?php
+    } else {
+      ?>
+      <p><strong>Players:</strong> Unknown</p>
+    <?php
+    }
+    ?>
 
+
+    <div class="progress progress-striped active">
+      <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="<?php
+      $percentageFilled = 0;
+      if ($serverStatus->isOnline() == true) {
+        $percentageFilled = $serverStatus->getOnlinePlayerCount() / $serverStatus->getMaxPlayerCount() * 100;
+      }
+      echo $percentageFilled;
+      ?>" aria-valuemin="0"
+           aria-valuemax="100" style="width: <?php echo $percentageFilled; ?>%">
+        <span class="sr-only"><?php echo $percentageFilled; ?>% Complete</span>
+      </div>
+    </div>
   </div>
 </div>
+
