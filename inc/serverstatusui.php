12d1a9eb2bb3b5823221dfa23ccd82876d2aec4c
Zachary DuBois <zacharydubois@icloud.com>
2014-12-24 16:20:31 -0500

    Finish adding back server queries.
    Closes #17.

--- inc/serverstatusui.php
+++ inc/serverstatusui.php
@@ -36,8 +36,18 @@ $r = $status->getStatus($mc_server_ip, $mc_server_port);
       <p><strong>Name:</strong> <?php echo $server_name; ?></p>
       <p><strong>IP:</strong> <?php echo $mc_server_disp_addr; ?></p>
 
+      <p><strong>Version:</strong> <?php echo $r['version']; ?></p>
       <p><strong>Players:</strong> <?php echo $r['players'] . " of " . $r['maxplayers'] . " max"; ?></p>
       <p><strong><abbr title="Message Of The Day">MOTD</abbr>:</strong> <?php echo $r['motd']; ?></p>
+
+      <div class="progress progress-striped active">
+        <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="<?php
+        $percentageFilled = $r['players'] / $r['maxplayers'] * 100;
+        echo $percentageFilled;
+        ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $percentageFilled; ?>%">
+          <span class="sr-only"><?php echo $percentageFilled; ?>% Complete</span>
+        </div>
+      </div>
     <?php } ?>
 
   </div>
