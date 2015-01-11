4e5f9a1ae8bd880e9707c8f5b7ae6a1440c68539
Zachary DuBois <zacharydubois@icloud.com>
2015-01-11 13:04:15 -0500

    Make debug nicer

--- inc/debug.php
+++ inc/debug.php
@@ -5,27 +5,59 @@
  */
 
 include_once APPPATH . 'config.php';
+include_once APPPATH . 'inc/version.php';
+
+if (file_exists(APPPATH . 'tmp/query.json')) {
+  $queryCache = json_decode(file_get_contents(APPPATH . 'tmp/query.json'), true);
+} else {
+  $queryCache = 'No Cache File';
+}
 
 $output = array(
-    'serverIP'          => $mc_server_ip,
-    'serverPort'        => $mc_server_port,
-    'serverName'        => $_SERVER['SERVER_NAME'],
-    'requestURI'        => $_SERVER['REQUEST_URI'],
-    'serverSoft'        => $_SERVER['SERVER_SOFTWARE'],
-    'jsonEnabled'       => extension_loaded("json"),
-    'cdnURI'            => $custom_hosted_uri,
-    'baseURI'           => $base_URL,
-    'avatarURI'         => $avatar_service_uri,
-    'customLinks'       => $custom_links,
-    'tmpDirExits'       => file_exists(APPPATH . 'tmp'),
-    'tmpDirWrite'       => is_writable(APPPATH . 'tmp'),
-    'configExits'       => file_exists(APPPATH . 'config.php'),
-    'configWrite'       => is_writable(APPPATH . 'config.php'),
-    'installExists'     => file_exists(APPPATH . 'pages/install/'),
-    'topStatCalType'    => $player_top_calc_stat,
-    'numberOfTopPlayer' => $player_on_top,
-
+    'serverInfo' => array(
+        'phpVersion'  => PHP_VERSION,
+        'webServer'   => $_SERVER['SERVER_SOFTWARE'],
+        'os'          => PHP_OS,
+        'serverName'  => $_SERVER['SERVER_NAME'],
+        'requestURI'  => $_SERVER['REQUEST_URI'],
+        'jsonEnabled' => extension_loaded("json"),
+        'mysqliEnabled' => extension_loaded("mysqli"),
+    ),
+    'configData' => array(
+        'serverIP'          => $mc_server_ip,
+        'serverPort'        => $mc_server_port,
+        'cdnURI'            => $custom_hosted_uri,
+        'baseURI'           => $base_URL,
+        'avatarURI'         => $avatar_service_uri,
+        'customLinks'       => $custom_links,
+        'topStatCalType'    => $player_top_calc_stat,
+        'numberOfTopPlayer' => $player_on_top,
+    ),
+    'envInfo'    => array(
+        'APPPATH'     => APPPATH,
+        'LINKBASE'    => LINKBASE,
+        'fmcsVersion' => $fmcs_version,
+    ),
+    'fileInfo'   => array(
+        'tmpDirExits'          => file_exists(APPPATH . 'tmp'),
+        'tmpDirWrite'          => is_writable(APPPATH . 'tmp'),
+        'configExits'          => file_exists(APPPATH . 'config.php'),
+        'configWrite'          => is_writable(APPPATH . 'config.php'),
+        'installExists'        => file_exists(APPPATH . 'pages/install/'),
+        'queryCacheFileExists' => file_exists(APPPATH . 'tmp/query.json'),
+        'queryCacheFileWrite'  => is_writable(APPPATH . 'tmp/query.json'),
+    ),
+    'otherInfo'  => array(
+        'queryData' => $queryCache,
+    ),
 );
 
-echo json_encode($output);
+header('Content-type: text/plain');
+
+echo "YOU ARE IN DEBUG MODE! FLUID MC STATS WILL NOT FUNCTION WHEN IN THIS MODE!\n";
+
+echo "Paste the following into your issue on GitHub:\n```json\n";
+echo json_encode($output, JSON_PRETTY_PRINT);
+echo "\n```";
+
 die();
