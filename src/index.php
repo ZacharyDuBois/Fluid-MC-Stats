a656b90af6150bd0f053b6be19cd2bbe964cd8df
Zachary DuBois <me@zacharydubois.me>
2016-03-31 17:43:32 -0400

    Create index.php

---
+++ src/index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * File: index.php
+ * User: zacharydubois
+ * Date: 2016-03-31
+ * Time: 17:33
+ * Project: Fluid-MC-Stats
+ */
+
+// Do not edit any of the following unless you know what you're doing.
+
+// Set version.
+define('VERSION', 'v0.3.0');
+
+// Shortcut directory separators.
+define('DS', DIRECTORY_SEPARATOR);
+
+// Define base path.
+define('PATH', __DIR__ . DS);
+
+// Define app path.
+define('APP', PATH . DS . 'fmcs' . DS);
+
+// Define config.
+define('CONFIG', PATH . DS . 'storage' . DS . 'config.php');
+
+// Define temporary directory.
+define('TMP', PATH . DS . 'temporary' . DS);
