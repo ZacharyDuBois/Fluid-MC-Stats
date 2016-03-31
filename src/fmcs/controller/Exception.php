3324ead56bc22f7c9cc13a57f13aa703ed416c10
Zachary DuBois <me@zacharydubois.me>
2016-03-31 19:02:39 -0400

    Create exception class.

---
+++ src/fmcs/controller/Exception.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * File: Exception.php
+ * User: zacharydubois
+ * Date: 2016-03-31
+ * Time: 18:56
+ * Project: Fluid-MC-Stats
+ */
+
+namespace fmcs;
+
+
+class Exception extends \Exception {
+
+}
